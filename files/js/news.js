3ffb12c85839eeeb47363bfb799c902610960049
lr0pb <hello.lr0pb@gmail.com>
2019-07-30 17:23:31 +0300

    News

---
+++ files/js/news.js
@@ -0,0 +1,20 @@
+$('#news-head').click(back);
+$(document).keydown(function(e) {
+  if(e.keyCode===66){
+    back();
+  };
+});
+function back() {
+  $('body').addClass('overflow-hidden');
+  $('#transition-layer').css({
+    'display':'block',
+    'animation':'1s transition-start-phase'
+  });
+  setTimeout(function() {
+    document.location.href = "https://lr0pb.github.io/fly";
+  }, 1000);
+  setTimeout(function() {
+    $('body').addClass('overflow-hidden');
+    $('#transition-layer').css('display','none');
+  }, 1100);
+};
