f611392617d1587e1264749dcfec4b808d6c3344
lr0pb <hello.lr0pb@gmail.com>
2019-07-31 14:25:12 +0300

    Upd

--- files/js/main.js
+++ files/js/main.js
@@ -1,4 +1,7 @@
 $('#burger').mousedown(function() {
   $('nav').toggleClass('nav-burger');
   $('a.norm').toggleClass('a-burger');
+  $('span:nth-child(1)').toggleClass('cross-left');
+  $('span:nth-child(2)').toggleClass('cross-right');
+  $('span:nth-child(3)').toggleClass('not-now');
 });
