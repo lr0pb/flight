1f7eff743c2398c797a4a14791f19bcd7976bb4f
lr0pb <hello.lr0pb@gmail.com>
2019-08-01 09:29:33 +0300

    Promo block experiments

--- files/js/first.js
+++ files/js/first.js
@@ -3,3 +3,13 @@ setTimeout(function() {
   $('#transition-layer').css('display','none');
   $('body').removeClass('overflow-hidden');
 }, 1000);
+$('#fly-promo-button').click(function() {
+  $('body').addClass('overflow-hidden');
+  $('#transition-layer').css({
+    'display':'block',
+    'animation':'1s transition-start-phase'
+  });
+  setTimeout(function() {
+    document.location.href = "https://lr0pb.github.io/fly";
+  }, 1000);
+});
