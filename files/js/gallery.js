00ff4cc83c880cfb790463ba4526094b16e09894
lr0pb <hello.lr0pb@gmail.com>
2019-09-28 12:39:31 +0300

    Upd

--- files/js/gallery.js
+++ files/js/gallery.js
@@ -1,5 +1,4 @@
 document.ready(function() {
-  $('body').addClass('overflow-hidden');
   setTimeout(function() {
     $('#transition-layer').css('display','none');
     $('body').removeClass('overflow-hidden');
