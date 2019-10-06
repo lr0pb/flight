6a3035adb9bf10099901c7f2c5d33bf86f100629
lr0pb <hello.lr0pb@gmail.com>
2019-10-06 22:20:03 +0300

    Upd

--- dev/files/js/fly.js
+++ dev/files/js/fly.js
@@ -770,37 +770,3 @@ function close() {
   if (fly.webkitRequestFullscreen) document.exitFullscreen();
   else if (fly.requestFullscreen) document.exitFullscreen();
 };
-
-$('#news-block').click(function() {
-  $('body').addClass('overflow-hidden');
-  $('#transition-layer').css({
-    'display':'block',
-    'animation':'1s transition-start-phase'
-  });
-  setTimeout(function() {
-    document.location.href = "https://flight.pp.ua/news";
-  }, 1000);
-});
-
-var circle = document.getElementById('circle');
-
-document.getElementById('news-block').addEventListener('click', function() {
-  const x = e.pageX;
-  const y = e.pageY;
-  circle.css({
-    'opacity':'1',
-    'transform-origin':`${x}, ${y}`,
-    'transform':'scale(5)';
-  });
-});
-
-$('#gallery-block').click(function() {
-  $('body').addClass('overflow-hidden');
-  $('#transition-layer').css({
-    'display':'block',
-    'animation':'1s transition-start-phase'
-  });
-  setTimeout(function() {
-    document.location.href = "https://flight.pp.ua/gallery";
-  }, 1000);
-});
