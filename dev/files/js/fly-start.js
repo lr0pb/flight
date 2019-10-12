e52b52e96dfc243003bbb4027a5692e187a85476
lr0pb <hello.lr0pb@gmail.com>
2019-10-12 17:33:56 +0300

    Upd

--- dev/files/js/fly-start.js
+++ dev/files/js/fly-start.js
@@ -12,16 +12,11 @@
 const circle = document.getElementById('circle');
 
 document.getElementById('news-block').addEventListener('click', function(e) {
+  circle.style.cssText = 'opacity: 1; transform: translate(-50%) scale(5);';
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
-  console.log(circle.style.left);
-  console.log(circle.style.top);
   circle.style.display = 'block';
-  console.log(circle.style.display);
-  setTimeout(function() {
-    circle.style.cssText = 'opacity: 1; transform: translate(-50%) scale(3);';
-    console.log(circle.style.display);
-  }, 15);
+  console.log(circle.style.cssText);
 });
 
 $('#gallery-block').click(function() {
