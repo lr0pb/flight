565971ce5efe892d0e9152d1e7f918cfe1ef10f1
lr0pb <hello.lr0pb@gmail.com>
2019-10-13 12:16:04 +0300

    Upd

--- dev/files/js/fly-start.js
+++ dev/files/js/fly-start.js
@@ -12,10 +12,12 @@
 const circle = document.getElementById('circle');
 
 document.getElementById('news-block').addEventListener('click', function(e) {
+  document.querySelector('body').classList.add('overflow-hidden');
   circle.style.display = 'block';
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
-  setTimeout( () => {circle.style.transform = 'translateX(-50%) translateY(-50%) scale(4)'}, 15 );
+  setTimeout( () => {circle.style.transform = 'translateX(-50%) translateY(-50%) scale(5)'}, 15 );
+  setTimeout( () => {document.location.href = 'https://flight.pp.ua/dev/news'}, 1200 );
 });
 
 $('#gallery-block').click(function() {
