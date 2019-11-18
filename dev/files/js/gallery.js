081377bf3574750a8dac4ecfebfe9043d9ccec22
lr0pb <hello.lr0pb@gmail.com>
2019-11-18 08:45:54 +0200

    Upd

--- dev/files/js/gallery.js
+++ dev/files/js/gallery.js
@@ -48,11 +48,16 @@ document.addEventListener('keydown', function(e) {
   if(e.keyCode===66) back();
 });
 
+document.getElementById('back-button').addEventListener('touchend', function(e) {
+  click(e);
+  setTimeout( () => {document.location.href = 'https://flight.pp.ua/dev/fly';}, 1200 );
+});
+
 function back() {
   document.querySelector('body').classList.add('overflow-hidden');
   circle.style.display = 'block';
   setTimeout( () => {circle.style.transform = 'translateX(-50%) translateY(-50%) scale(5)'; circle.style.opacity = '1'}, 15);
-  setTimeout( () => {document.location.href = "https://flight.pp.ua/dev/fly";}, 1200 );
+  setTimeout( () => {document.location.href = 'https://flight.pp.ua/dev/fly';}, 1200 );
 };
 
 let i = 0;
