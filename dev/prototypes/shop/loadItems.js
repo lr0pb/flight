2d6509408112cf75ac6c38e2b7ddfc3998613dc4
lr0pb <hello.lr0pb@gmail.com>
2020-01-13 18:01:43 +0200

    Upd

--- dev/prototypes/shop/loadItems.js
+++ dev/prototypes/shop/loadItems.js
@@ -12,8 +12,9 @@ document.addEventListener('DOMContentLoaded', async function () {
   loadItems();
 });
 
+
 document.addEventListener('scroll', function () {
-  if (document.documentElement.scrollTop % 1200 >= 1 && document.documentElement.scrollTop % 1200 <= 300) {
+  if (document.documentElement.scrollTop > 1200 && document.documentElement.scrollTop % 1200 >= 1 && document.documentElement.scrollTop % 1200 <= 300) {
     loadItems();
   };
 });
