40f3d915bf398ecceb03dc819b55f0d6ac9fde60
lr0pb <hello.lr0pb@gmail.com>
2020-01-13 17:23:39 +0200

    Upd

--- dev/prototypes/shop/loadItems.js
+++ dev/prototypes/shop/loadItems.js
@@ -1,22 +1,19 @@
-let arr = [
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-  {title: 'Futbolka', price: 7, image: 'image', link: 'link'},
-]
-
+let i
 let response
 
+document.addEventListener('DOMContentLoaded', async function () {
+  for (let j = 0; j < 30; j++) {
+    response = await fetch(`items/itemList${j}.json`);
+    if (!response.ok) {
+      i = j--;
+      break;
+    };
+  };
+  loadItems();
+});
+
 async function loadItems() {
-  response = await fetch('items/itemList1.json');
+  response = await fetch(`items/itemList${i}.json`);
 
   if (response.ok) {
     let rawJson = await response.json();
@@ -25,23 +22,24 @@ async function loadItems() {
   } else {
     console.log(response.status);
   };
-}
-loadItems()
+
+  i--;
+};
 
 function showItems(json) {
-  for (let i = 0; i < json.length; i++) {
+  for (let k = 0; k < json.length; k++) {
     let item = document.createElement('div');
     item.className = 'item';
-    item.dataset.link = json[i].link;
+    item.dataset.link = json[k].link;
     let image = document.createElement('div');
-    image.style.backgroundImage = '/images/' + json[i].image + '.jpg';
+    image.style.backgroundImage = 'url(/images/' + json[k].image + '.jpg)';
     let title = document.createElement('h3');
-    title.textContent = json[i].title;
+    title.textContent = json[k].title;
     let price = document.createElement('h4');
-    price.textContent = json[i].price;
+    price.textContent = json[k].price;
     item.append(image);
     item.append(title);
     item.append(price);
     document.querySelector('#itemsList').append(item);
   };
-}
+};
