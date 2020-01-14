3fb574580f7b53b1df66dd4c77c8edc0841e6f77
lr0pb <hello.lr0pb@gmail.com>
2020-01-14 16:23:11 +0200

    Upd

--- dev/prototypes/shop/loadItems.js
+++ dev/prototypes/shop/loadItems.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', async function () {
   async function iDetecter() {
     for (let j = 1; j < 30; j++) {
       response = await fetch(`items/itemList${j}.json`);
-      console.log(response);
       if (!response.ok) {
         i = --j;
         break;
@@ -19,7 +18,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 });
 
 document.addEventListener('scroll', async function () {
-  if (document.documentElement.scrollTop > 1250 && document.documentElement.scrollTop % 1250 > 0 && document.documentElement.scrollTop % 1250 < 100) {
+  if (document.documentElement.scrollTop > 1250 && document.documentElement.scrollTop % 1250 > 0 && document.documentElement.scrollTop % 1250 < 35) {
     document.querySelector('#loadingBlock').style.display = 'flex';
     await loadItems();
     document.querySelector('#loadingBlock').style.display = 'none';
